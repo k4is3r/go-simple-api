13dc2c0f55581694852be9a9e1ea8db69a0aa62c
Eduardo Imery <ejwimery@gmail.com>
2020-01-26 20:48:42 -0600

    Adding some data to try the api

--- main.go
+++ main.go
@@ -19,8 +19,11 @@ type Address struct {
   Stater string `json:"state.omitempy"`
 }
 
-func GetPeopleEndpoint(w http.ResponseWriter, req *http.Request){
+var people []Person
 
+
+func GetPeopleEndpoint(w http.ResponseWriter, req *http.Request){
+  
 }
 
 func GetPersonEndpoint(w http.ResponseWriter, req *http.Request){
@@ -39,7 +42,15 @@ func main(){
   #routers
   router := mux.NewRouter()
 
+  people = append(people, Person{ID: "1", FirstName: "Edward", LastName: "Ime", Address: &Address{ City: "Dublin", State: "Casto"}})
 
+people = append(people, Person{ID: "2", FirstName: "Caro", LastName: "Hernandez", Address: &Address{ City: "SanFrancisco", State: "California"}})
+
+people = append(people, Person{ID: "3", FirstName: "Samantha", LastName: "Rueda", Address: &Address{ City: "Spain", State: "Madrid"}})
+
+people = append(people, Person{ID: "4", FirstName: "MariaDelCarmen", LastName: "De Abreu", Address: &Address{ City: "Santiago", State: "Chile"}})
+
+people = append(people, Person{ID: "5", FirstName: "Eduardo", LastName: "Wintetdaal", Address: &Address{ City: "Holanda", State: "Amsterdan"}})
 
   #Endpoints
   router.HandleFunc("/people", GetPeopleEndpoint).Methods("GET")
